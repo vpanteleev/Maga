f425363454894fdeb157056024cc94856d45577b
Vladimir <vladimirpanteleev@Vladimirs-MacBook-Pro.local>
2017-11-19 01:20:41 +0300

    Add destroyed boxes

---
+++ TVIDR/Udacity-Carnival-Starter-Project-master/Assets/test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	public GameObject destroyedVersion;
+	// Use this for initialization
+	public void destroyThisObject () {
+		Instantiate(destroyedVersion, transform.position, transform.rotation);
+		Destroy(gameObject);
+
+	}
+}
